68d42e9db296dbf73a5ce1535281085e5017e561
50516019 <50516019@50516019-pc>
2017-11-06 14:33:40 +0900

    matrix_kadai

--- src/matrix/Matrix_main.java
+++ src/matrix/Matrix_main.java
@@ -6,10 +6,11 @@ public class Matrix_main {
 	
 	public static void main(String[] args) {
 	
-	  double a[][] = {{1, 2}, {-1,1}};
-	  double b[][] = {{3, -1}, {2, 1}};
+	  double a[][] = {{0, 1}, {2, 0}, {0, 3}};
+	  double b[][] = {{1, -1, 2}, {3, 0, 4}};
 	  
 	  Matrix_lib mlib = new Matrix_lib(a, b);
 	  System.out.println("Prodct = "+Arrays.deepToString(mlib.getProduct()));
+	  System.out.println("Prodct = "+Arrays.deepToString(mlib.getTranspose(b)));
 	}
 }
