a0748a43dc3be67d574a75c6fa2f1e46d03afdb5
50516019 <50516019@50516019-pc>
2017-11-13 01:28:41 +0900

    11/6課題

--- src/matrix/Matrix_main.java
+++ src/matrix/Matrix_main.java
@@ -1,16 +1,18 @@
-package matrix;
+package matrix; 
 
-import java.util.Arrays;
+import java.util.Arrays; 
+public class Matrix_main { 
+	public static void main(String[] args) { 
 
-public class Matrix_main {
-	
-	public static void main(String[] args) {
-	
-	  double a[][] = {{0, 1}, {2, 0}, {0, 3}};
-	  double b[][] = {{1, -1, 2}, {3, 0, 4}};
-	  
-	  Matrix_lib mlib = new Matrix_lib(a, b);
-	  System.out.println("Prodct = "+Arrays.deepToString(mlib.getProduct()));
-	  System.out.println("Prodct = "+Arrays.deepToString(mlib.getTranspose(b)));
-	}
+ 		double a[][] = { { 1, 2 }, { -1, 1 }}; 
+ 		double b[][] = { { 3, -1 }, { 2, 4 } }; 
+ 		double c[][] = {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}}; 
+		 
+  		Matrix_lib mlib = new Matrix_lib(); 
+ 	    System.out.println("Product = " +Arrays.deepToString(mlib.getProduct(a,b))); 
+     	System.out.println("Transpose = " +Arrays.deepToString(mlib.getTranspose(b))); 
+		System.out.println("Cofactor = " +mlib.getCofactor(c)); 
+		System.out.println("Inverse = " +Arrays.deepToString(mlib.getInverse(c))); 
+ 
+ } 
 }
